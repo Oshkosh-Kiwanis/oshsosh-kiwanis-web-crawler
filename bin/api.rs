2dc895b087aa748e8031e061c3b063f55ab830d1
Jason Cardinal <jason.cardinal@timiosdevelopment.com>
2021-08-19 03:19:15 -0500

    don't bind to the loopback interface

--- bin/api.rs
+++ bin/api.rs
@@ -85,7 +85,7 @@ async fn index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, E
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().route("/ws/", web::get().to(index)))
-        .bind("127.0.0.1:80")?
+        .bind("0.0.0.0:80")?
         .run()
         .await
 }
